38ad64a1d0497dd772e0b95ade442aa4adb95129
Dan Hudson <dan@orphans.co.uk>
2026-05-19 13:09:14 +0100

    New: update icons for carousel

---
+++ src/components/icons/breakpoints.js
@@ -0,0 +1,86 @@
+import { SVG, Path, Polygon, G, Rect, Line } from '@wordpress/components';
+
+export const mobile = (
+	<SVG
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width="24"
+		height="24"
+		aria-hidden="true"
+		focusable="false"
+	>
+		<Path d="M15 4H9c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h6c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm.5 14c0 .3-.2.5-.5.5H9c-.3 0-.5-.2-.5-.5V6c0-.3.2-.5.5-.5h6c.3 0 .5.2.5.5v12zm-4.5-.5h2V16h-2v1.5z"></Path>
+	</SVG>
+);
+
+export const largeMobile = (
+	<SVG
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width="24"
+		height="24"
+		aria-hidden="true"
+		focusable="false"
+	>
+		<G transform="rotate(-90 12 12)">
+			<Path d="M15 4H9c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h6c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm.5 14c0 .3-.2.5-.5.5H9c-.3 0-.5-.2-.5-.5V6c0-.3.2-.5.5-.5h6c.3 0 .5.2.5.5v12zm-4.5-.5h2V16h-2v1.5z"></Path>
+		</G>
+	</SVG>
+);
+
+export const tablet = (
+	<SVG
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width="24"
+		height="24"
+		aria-hidden="true"
+		focusable="false"
+	>
+		<Path d="M17 4H7c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm.5 14c0 .3-.2.5-.5.5H7c-.3 0-.5-.2-.5-.5V6c0-.3.2-.5.5-.5h10c.3 0 .5.2.5.5v12zm-7.5-.5h4V16h-4v1.5z"></Path>
+	</SVG>
+);
+
+export const laptop = (
+	<SVG
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width="24"
+		height="24"
+		aria-hidden="true"
+		focusable="false"
+	>
+		<Path d="M20.5 16h-.7V8c0-1.1-.9-2-2-2H6.2c-1.1 0-2 .9-2 2v8h-.7c-.8 0-1.5.7-1.5 1.5h20c0-.8-.7-1.5-1.5-1.5zM5.7 8c0-.3.2-.5.5-.5h11.6c.3 0 .5.2.5.5v7.6H5.7V8z"></Path>
+	</SVG>
+);
+
+export const desktop = (
+	<SVG
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width="24"
+		height="24"
+		aria-hidden="true"
+		focusable="false"
+		fill="none"
+		stroke="currentColor"
+		strokeWidth="1.5"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+	>
+		<Rect
+			x="2"
+			y="4"
+			width="20"
+			height="12"
+			rx="1.5"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		></Rect>
+		<Line x1="10" y1="18" x2="14" y2="18" />
+		<Line x1="8" y1="20" x2="16" y2="20" />
+	</SVG>
+);
--- src/constants/breakpoints/index.js
+++ src/constants/breakpoints/index.js
@@ -1,17 +1,20 @@
-import { mobile, tablet, desktop } from '@wordpress/icons';
+import {
+	mobile,
+	largeMobile,
+	tablet,
+	laptop,
+	desktop,
+} from '../../components/icons/breakpoints';
 
-import breakpointsData, {
-	breakpointTokens,
-	emblaMinWidthQuery,
-} from './data';
+import breakpointsData, { breakpointTokens, emblaMinWidthQuery } from './data';
 
 export { breakpointTokens, emblaMinWidthQuery };
 
 const breakpoints = {
 	xs: { ...breakpointsData.xs, icon: mobile },
-	sm: { ...breakpointsData.sm, icon: tablet },
+	sm: { ...breakpointsData.sm, icon: largeMobile },
 	md: { ...breakpointsData.md, icon: tablet },
-	lg: { ...breakpointsData.lg, icon: desktop },
+	lg: { ...breakpointsData.lg, icon: laptop },
 	xl: { ...breakpointsData.xl, icon: desktop },
 };
 
