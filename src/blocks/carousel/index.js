d72c8d5f8d9450a60e7c95c7efc94bc820502cb8
Dan Hudson <dan@orphans.co.uk>
2026-05-19 13:09:14 +0100

    New: update icons for carousel

--- src/blocks/carousel/index.js
+++ src/blocks/carousel/index.js
@@ -5,8 +5,10 @@ import './style.scss';
 import Edit from './edit';
 import Save from './save';
 import metadata from './block.json';
+import { Carousel } from '../../components/icons/carousel';
 
 registerBlockType(metadata.name, {
+	icon: Carousel,
 	edit: Edit,
 	save: Save,
 });
