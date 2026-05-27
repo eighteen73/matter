f21f427ec43f48fcf491f2ba0ddb7b4bc9798773
Dan Hudson <dan@orphans.co.uk>
2026-05-27 12:50:27 +0100

    New: post carousel variation

--- src/blocks/carousel/variations.js
+++ src/blocks/carousel/variations.js
@@ -48,6 +48,62 @@ const variations = [
 		],
 		isActive: ['className'],
 	},
+	{
+		name: 'post-carousel',
+		title: 'Post Carousel',
+		description: 'Carousel with post slides and navigation controls',
+		attributes: {
+			className: 'is-style-post-carousel',
+		},
+		innerBlocks: [
+			[
+				'eighteen73-blocks/carousel-viewport',
+				{
+					allowedBlocks: ['core/query'],
+					lock: { remove: true },
+				},
+				[
+					[
+						'core/query',
+						{
+							query: {
+								perPage: 10,
+								pages: 0,
+								offset: 0,
+								postType: 'post',
+								inherit: false,
+							},
+							lock: { remove: true },
+						},
+						[['core/post-template', {}, [['core/post-title']]]],
+					],
+				],
+			],
+			[
+				'eighteen73-blocks/carousel-progress',
+				{
+					indicateCurrentPosition: true,
+				},
+			],
+			[
+				'core/group',
+				{
+					layout: {
+						type: 'flex',
+						justifyContent: 'space-between',
+						flexWrap: 'nowrap',
+						verticalAlignment: 'center',
+					},
+				},
+				[
+					['eighteen73-blocks/carousel-previous-button'],
+					['eighteen73-blocks/carousel-dots'],
+					['eighteen73-blocks/carousel-next-button'],
+				],
+			],
+		],
+		isActive: ['className'],
+	},
 ];
 
 export default variations;
