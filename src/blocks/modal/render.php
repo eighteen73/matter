2354213468bc3c8a56748f92685aa9877abf330a
Brett Mason <brett@eighteen73.co.uk>
2026-06-23 09:42:29 +0100

    Add additional functionality to modal block

--- src/blocks/modal/render.php
+++ src/blocks/modal/render.php
@@ -5,8 +5,10 @@
  *     $content (string): The block default content.
  *     $block (WP_Block): The block instance.
  *
- * @package Matter\\Modal
+ * @package Eighteen73\Matter\Modal
  */
+
+use Eighteen73\Matter\Blocks\Modal;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -26,13 +28,36 @@ if ( empty( $modal_id ) ) {
 	$modal_id = wp_unique_id( 'matter-modal-' );
 }
 
+$dismissed_duration = ! empty( $block_attributes['dismissedDuration'] )
+	? sanitize_text_field( (string) $block_attributes['dismissedDuration'] )
+	: '';
+$trigger_on_load    = ! empty( $block_attributes['triggerOnLoad'] );
+$trigger_delay      = ! empty( $block_attributes['triggerDelay'] )
+	? sanitize_text_field( (string) $block_attributes['triggerDelay'] )
+	: '';
+$trigger_on_scroll  = ! empty( $block_attributes['triggerOnScroll'] );
+$scroll_selector    = ! empty( $block_attributes['scrollSelector'] )
+	? sanitize_text_field( (string) $block_attributes['scrollSelector'] )
+	: '';
+$scroll_threshold   = ! empty( $block_attributes['scrollThreshold'] )
+	? sanitize_text_field( (string) $block_attributes['scrollThreshold'] )
+	: '10';
+$url_triggers       = Modal::sanitize_url_triggers( $block_attributes['urlTriggers'] ?? [] );
+
 wp_interactivity_state(
 	'matter/overlay/private',
 	[
 		'items' => [
 			$modal_id => [
-				'isOpen' => false,
-				'type'   => 'modal',
+				'isOpen'            => false,
+				'type'              => 'modal',
+				'dismissedDuration' => $dismissed_duration,
+				'triggerOnLoad'     => $trigger_on_load,
+				'triggerDelay'      => $trigger_delay,
+				'triggerOnScroll'   => $trigger_on_scroll,
+				'scrollSelector'    => $scroll_selector,
+				'scrollThreshold'   => $scroll_threshold,
+				'urlTriggers'       => $url_triggers,
 			],
 		],
 	]
@@ -45,6 +70,7 @@ wp_interactivity_state(
 		get_block_wrapper_attributes(
 			[
 				'data-wp-interactive' => 'matter/overlay',
+				'data-wp-init'        => 'callbacks.onInit',
 			]
 		)
 		. ' '
