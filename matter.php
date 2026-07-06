2af267c5a18afeb5cb6e4f69120c52ce08c0006d
Dan Hudson <dan@orphans.co.uk>
2026-07-06 15:41:21 +0100

    New: bump version number

--- matter.php
+++ matter.php
@@ -4,7 +4,7 @@
  * Plugin URI:        https://eighteen73.co.uk
  * Update URI:        https://eighteen73.co.uk
  * Description:       A collection of expertly crafted blocks for WordPress.
- * Version:           0.1.5
+ * Version:           0.1.6
  * Requires at least: 6.8
  * Requires PHP:      7.4
  * Requires Plugins:
