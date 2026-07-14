bb49bd6d071dfbfe4bbf122c44cb16747a4f9eea
Dan Hudson <dan@orphans.co.uk>
2026-07-14 09:14:11 +0100

    New: accordion first pass

---
+++ src/blocks/accordion-panel/edit.js
@@ -0,0 +1,73 @@
+/**
+ * WordPress dependencies
+ */
+import {
+	useBlockProps,
+	useInnerBlocksProps,
+	store as blockEditorStore,
+} from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+import { useSelect } from '@wordpress/data';
+import clsx from 'clsx';
+
+const TEMPLATE = [
+	[
+		'core/paragraph',
+		{
+			placeholder: __('Type / to add content…', 'matter'),
+		},
+	],
+];
+
+const QUERY_TEMPLATE = [
+	['core/post-featured-image', { isLink: true, aspectRatio: '16/9' }],
+	['core/post-title'],
+	['core/post-excerpt'],
+];
+
+export default function Edit({ attributes, clientId, context }) {
+	const { templateLock } = attributes;
+	const isQueryMode = context['matter/accordion-isQueryMode'] ?? false;
+	const openByDefault = context['matter/accordion-open-by-default'] ?? false;
+
+	const { isSelected } = useSelect(
+		(select) => {
+			const { isBlockSelected, hasSelectedInnerBlock, getBlockParents } =
+				select(blockEditorStore);
+
+			if (openByDefault || isQueryMode) {
+				return { isSelected: true };
+			}
+
+			const parents = getBlockParents(clientId);
+			const itemClientId = parents[parents.length - 1];
+			const itemSelected =
+				itemClientId &&
+				(isBlockSelected(itemClientId) ||
+					hasSelectedInnerBlock(itemClientId, true));
+
+			return {
+				isSelected:
+					isBlockSelected(clientId) ||
+					hasSelectedInnerBlock(clientId, true) ||
+					!!itemSelected,
+			};
+		},
+		[clientId, openByDefault, isQueryMode]
+	);
+
+	const blockProps = useBlockProps({
+		role: 'region',
+		className: clsx({
+			'is-open': openByDefault || isSelected,
+		}),
+	});
+
+	const innerBlocksProps = useInnerBlocksProps(blockProps, {
+		template: isQueryMode ? QUERY_TEMPLATE : TEMPLATE,
+		templateLock: templateLock || false,
+		templateInsertUpdatesSelection: true,
+	});
+
+	return <div {...innerBlocksProps} />;
+}
---
+++ src/blocks/accordion/edit.js
@@ -0,0 +1,225 @@
+/**
+ * WordPress dependencies
+ */
+import {
+	useBlockProps,
+	useInnerBlocksProps,
+	InspectorControls,
+	BlockControls,
+	HeadingLevelDropdown,
+	store as blockEditorStore,
+} from '@wordpress/block-editor';
+import {
+	ToggleControl,
+	Notice,
+	ToolbarButton,
+	ToolbarGroup,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalToolsPanel as ToolsPanel,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalToolsPanelItem as ToolsPanelItem,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalToggleGroupControl as ToggleGroupControl,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalToggleGroupControlOption as ToggleGroupControlOption,
+} from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { createBlock } from '@wordpress/blocks';
+import { useEffect } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import './editor.scss';
+import BlockVariationPicker from '../../components/block-variation-picker';
+import useBlockId from '../../utils/use-block-id';
+import { ITEM_TEMPLATE } from './variations';
+
+const DEFAULT_TEMPLATE = [ITEM_TEMPLATE];
+
+function Edit({ clientId, attributes, setAttributes, isSelected }) {
+	const { autoclose, iconPosition, showIcon, headingLevel, isQueryMode } =
+		attributes;
+
+	const { insertBlock } = useDispatch(blockEditorStore);
+
+	const { duplicateAnchor } = useBlockId({
+		blockName: 'matter/accordion',
+		prefix: 'matter-accordion',
+		attributes,
+		setAttributes,
+		clientId,
+	});
+
+	const innerBlocks = useSelect(
+		(select) => select(blockEditorStore).getBlocks(clientId),
+		[clientId]
+	);
+
+	const hasQueryLoop = innerBlocks.some(
+		(block) => block.name === 'core/query'
+	);
+	const isQuery = isQueryMode || hasQueryLoop;
+
+	useEffect(() => {
+		if (hasQueryLoop !== isQueryMode) {
+			setAttributes({ isQueryMode: hasQueryLoop });
+		}
+	}, [hasQueryLoop, isQueryMode, setAttributes]);
+
+	const blockProps = useBlockProps({
+		role: 'group',
+	});
+
+	const innerBlocksProps = useInnerBlocksProps(blockProps, {
+		template: isQuery ? undefined : DEFAULT_TEMPLATE,
+		templateInsertUpdatesSelection: true,
+		defaultBlock: isQuery ? undefined : { name: 'matter/accordion-item' },
+		directInsert: !isQuery,
+		renderAppender: false,
+		allowedBlocks: isQuery ? ['core/query'] : ['matter/accordion-item'],
+	});
+
+	const addAccordionItem = () => {
+		const newItem = createBlock('matter/accordion-item', {}, [
+			createBlock('matter/accordion-heading', {}),
+			createBlock('matter/accordion-panel', {}, [
+				createBlock('core/paragraph', {
+					placeholder: __('Type / to add content…', 'matter'),
+				}),
+			]),
+		]);
+		insertBlock(newItem, undefined, clientId);
+	};
+
+	if (innerBlocks.length === 0) {
+		return (
+			<BlockVariationPicker
+				blockName="matter/accordion"
+				setAttributes={setAttributes}
+				clientId={clientId}
+				defaultTemplate={DEFAULT_TEMPLATE}
+			/>
+		);
+	}
+
+	return (
+		<>
+			{duplicateAnchor && (
+				<Notice status="warning" isDismissible={false}>
+					{__(
+						'Another accordion block is using this anchor. Choose a unique anchor so programmatic controls target the correct accordion.',
+						'matter'
+					)}
+				</Notice>
+			)}
+			{isSelected && !isQuery && (
+				<BlockControls>
+					<ToolbarGroup>
+						<ToolbarButton onClick={addAccordionItem}>
+							{__('Add item', 'matter')}
+						</ToolbarButton>
+					</ToolbarGroup>
+				</BlockControls>
+			)}
+			<BlockControls group="block">
+				<HeadingLevelDropdown
+					value={headingLevel}
+					onChange={(value) => setAttributes({ headingLevel: value })}
+				/>
+			</BlockControls>
+			<InspectorControls>
+				<ToolsPanel
+					label={__('Settings', 'matter')}
+					resetAll={() => {
+						setAttributes({
+							autoclose: false,
+							showIcon: true,
+							iconPosition: 'right',
+						});
+					}}
+				>
+					<ToolsPanelItem
+						hasValue={() => !!autoclose}
+						label={__('Auto-close', 'matter')}
+						onDeselect={() => setAttributes({ autoclose: false })}
+						isShownByDefault
+					>
+						<ToggleControl
+							__nextHasNoMarginBottom
+							label={__('Auto-close', 'matter')}
+							help={__(
+								'Automatically close accordion items when a new one is opened.',
+								'matter'
+							)}
+							checked={autoclose}
+							onChange={(value) =>
+								setAttributes({ autoclose: value })
+							}
+						/>
+					</ToolsPanelItem>
+					<ToolsPanelItem
+						hasValue={() => !showIcon}
+						label={__('Show icon', 'matter')}
+						onDeselect={() => setAttributes({ showIcon: true })}
+						isShownByDefault
+					>
+						<ToggleControl
+							__nextHasNoMarginBottom
+							label={__('Show icon', 'matter')}
+							help={__(
+								'Display a plus icon next to the accordion heading.',
+								'matter'
+							)}
+							checked={showIcon}
+							onChange={(value) =>
+								setAttributes({
+									showIcon: value,
+									iconPosition: value
+										? iconPosition
+										: 'right',
+								})
+							}
+						/>
+					</ToolsPanelItem>
+					{showIcon && (
+						<ToolsPanelItem
+							hasValue={() => iconPosition !== 'right'}
+							label={__('Icon position', 'matter')}
+							onDeselect={() =>
+								setAttributes({ iconPosition: 'right' })
+							}
+							isShownByDefault
+						>
+							<ToggleGroupControl
+								__nextHasNoMarginBottom
+								__next40pxDefaultSize
+								label={__('Icon position', 'matter')}
+								value={iconPosition}
+								onChange={(value) =>
+									setAttributes({
+										iconPosition: value,
+									})
+								}
+								isBlock
+							>
+								<ToggleGroupControlOption
+									value="left"
+									label={__('Left', 'matter')}
+								/>
+								<ToggleGroupControlOption
+									value="right"
+									label={__('Right', 'matter')}
+								/>
+							</ToggleGroupControl>
+						</ToolsPanelItem>
+					)}
+				</ToolsPanel>
+			</InspectorControls>
+			<div {...innerBlocksProps} />
+		</>
+	);
+}
+
+export default Edit;
