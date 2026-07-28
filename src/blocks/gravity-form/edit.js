ef1a26fc08e27928010b4f909ad6710ade3e2152
Dan Hudson <dan@orphans.co.uk>
2026-07-28 16:46:47 +0100

    New: rename icon import

--- src/blocks/gravity-form/edit.js
+++ src/blocks/gravity-form/edit.js
@@ -1,7 +1,7 @@
 import { useBlockProps } from '@wordpress/block-editor';
 import { Disabled, Notice, Spinner } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { GravityForm } from '../../components/icons/gravity-form';
+import { GravityForm as GravityFormIcon } from '../../components/icons/gravity-form';
 import { useServerSideRender } from '@wordpress/server-side-render';
 import useBlockId from '../../utils/use-block-id';
 import GravityFormInspectorControls from './components/inspector-controls';
@@ -60,7 +60,7 @@ export default function Edit({ attributes, setAttributes, clientId }) {
 		(form) => String(form.id) === String(formId)
 	);
 	const hasInvalidFormId = Boolean(formId) && !selectedForm;
-	const blockIcon = GravityForm;
+	const blockIcon = GravityFormIcon;
 
 	useBlockId({
 		blockName: 'matter/gravity-form',
